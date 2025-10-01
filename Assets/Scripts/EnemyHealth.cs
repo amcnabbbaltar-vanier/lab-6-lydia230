426c6a3b8902e2e2323421e17100165c58b9f7aa
lydia230 <lydia.ay4.la@gmail.com>
2025-10-01 13:52:57 -0400

    Confused at menuController

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+    public Slider healthBar;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = currentHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.value = currentHealth;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("The enemy has died");
+    }
+}
